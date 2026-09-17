527a0ae83e2765f06b3d09f256af78087e9f8e53
Abdu1Ja1a1 <raoua008.213@gmail.com>
2026-09-17 20:18:04 +0100

    added S3 bucket and terraform backend

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
     region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "abdultestbucket34334" {
+  bucket = "abdultestbucket343334"
+}
+terraform {
+  backend "s3" {
+    bucket         = "abdultestbucket343334"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
