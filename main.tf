c7229950656b9e7d35701d1ae40a3cb1dd00710a
Abdu1Ja1a1 <raoua008.213@gmail.com>
2026-09-17 20:31:35 +0100

    added bucket

--- main.tf
+++ main.tf
@@ -4,6 +4,9 @@ provider "aws" {
 resource "aws_s3_bucket" "abdultestbucket34334" {
   bucket = "abdultestbucket343334"
 }
+resource "aws_s3_bucket" "abdultestbucket343343" {
+  bucket = "abdultestbucket3433343"
+}
 terraform {
   backend "s3" {
     bucket         = "abdultestbucket343334"
